453f34c2338380a829ec779d2284754e06983be1
Radenavarro <rauldelapaznavarro@gmail.com>
2024-11-06 11:13:12 +0100

    adaptar confeti a react native reanimated

--- components/Confetti.tsx
+++ components/Confetti.tsx
@@ -1,5 +1,6 @@
-import React, { useEffect, useRef } from 'react';
-import { View, StyleSheet, Dimensions, Animated, Easing } from 'react-native';
+import React, { useEffect } from 'react';
+import { View, StyleSheet, Dimensions } from 'react-native';
+import Animated, { Easing, useSharedValue, withTiming, interpolate, useDerivedValue } from 'react-native-reanimated';
 
 type ConfettiPieceProps = {
   startPos: number;
@@ -15,31 +16,34 @@ const randomColor = () => {
 };
 
 const ConfettiPiece = ({ startPos }: ConfettiPieceProps) => {
-  const animatedValue = useRef(new Animated.Value(0)).current;
+
+  const animatedValue = useSharedValue(0)
+  const animation = withTiming(
+    1, 
+    {
+      duration: 3000 + Math.random() * 2000, 
+      easing: Easing.out(Easing.ease)
+    }
+  )
 
   useEffect(() => {
-    Animated.timing(animatedValue, {
-      toValue: 1,
-      duration: 3000 + Math.random() * 2000,
-      easing: Easing.out(Easing.ease),
-      useNativeDriver: true,
-    }).start();
-  }, []);
+    animatedValue.value = animation
+  }, [])
 
-  const translateY = animatedValue.interpolate({
-    inputRange: [0, 1],
-    outputRange: [-CONFETTI_SIZE, screenHeight],
-  });
+  const translateY = useDerivedValue(() => {
+    return interpolate(animatedValue.value, [0, 1], [-CONFETTI_SIZE, screenHeight])
+  })
 
-  const translateX = animatedValue.interpolate({
-    inputRange: [0, 1],
-    outputRange: [startPos, startPos + (Math.random() - 0.5) * 200],
-  });
+  const initialX = startPos + (Math.random() - 0.5) * screenWidth / 2;// Necesita estar fuera de useDerivedValue para que cada trozo de confeti conserve el valor y no se mueva por toda la pantalla
+  const confettiMoveX = (Math.random() - 0.5) * 200;
+  const translateX = useDerivedValue(() => {  
+    return interpolate(animatedValue.value, [0, 1], [initialX, initialX + confettiMoveX])
+  })
 
-  const rotate = animatedValue.interpolate({
-    inputRange: [0, 1],
-    outputRange: ['0deg', `${360 * (Math.random() > 0.5 ? 1 : -1)}deg`],
-  });
+  const rotationDeg = 360 * (Math.random() > 0.5 ? 1 : -1)
+  const rotate = useDerivedValue(() => {
+    return `${interpolate(animatedValue.value, [0, 1], [0, rotationDeg])}deg`
+  })
 
   return (
     <Animated.View
